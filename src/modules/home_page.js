d363580532d4e01d6166f3b83bab58f178be69ba
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2024-05-04 19:57:40 +0200

    Change name of variable 'container' to 'homepage'

---
+++ src/modules/home_page.js
@@ -0,0 +1,34 @@
+import image from "../images/restaurant.jpg";
+
+const homePage = () => {
+    
+    // -- Create elements --
+    const homepage = document.createElement("div");
+    container.classList.add("homepage");
+
+    const img = new Image();
+    img.src = image;
+    img.classList.add("homepage__img");
+
+    const textTitle = document.createElement("p");
+    textTitle.innerHTML = "Argentinian steak house";
+    textTitle.classList.add("homepage__text1");
+
+    const textParragraph1 = document.createElement("p");
+    textParragraph1.innerHTML = "Welcome to baires, where culinary excellence meets warm hospitality.";
+    textParragraph1.classList.add("homepage__text2");
+
+    const textParragraph2 = document.createElement("p");
+    textParragraph2.innerHTML = "Our story is one of passion, dedication, and a commitment to delivering an exceptional dining experience to our cherished guests."
+    textParragraph2.classList.add("homepage__text2");
+
+    // -- Append elements --
+    container.appendChild(img);
+    container.appendChild(textTitle);
+    container.appendChild(textParragraph1);
+    container.appendChild(textParragraph2);
+
+    return homepage;
+}
+
+export default homePage;
