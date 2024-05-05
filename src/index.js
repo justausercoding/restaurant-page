7706cd3d5ab228fa04226986602d5442080fae34
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2024-05-05 19:17:21 +0200

    Add event handlers for buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import menuPage from "./modules/menu_page.js";
 import contactPage from "./modules/contact-page.js"
 
 const content = document.querySelector("#content");
+const homeBtn = document.querySelector(".header__btn-home");
+const menuBtn = document.querySelector(".header__btn-menu");
+const contactBtn = document.querySelector(".header__btn-contact");
+
 content.appendChild(homePage());
-content.appendChild(menuPage());
-content.appendChild(contactPage());
+
+homeBtn.addEventListener("click", () => {
+    content.removeChild(content.firstChild);
+    content.appendChild(homePage());
+})
+
+menuBtn.addEventListener("click", () => {
+    content.removeChild(content.firstChild);
+    content.appendChild(menuPage());
+})
+
+contactBtn.addEventListener("click", () => {
+    content.removeChild(content.firstChild);
+    content.appendChild(contactPage());
+})
