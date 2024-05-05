c26e338b9f03401bb3d2aac76570f654a5932642
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2024-05-05 16:33:19 +0200

    Import and append the contact page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 import homePage from "./modules/home_page.js";
 import menuPage from "./modules/menu_page.js";
+import contactPage from "./modules/contact-page.js"
 
 const content = document.querySelector("#content");
 content.appendChild(homePage());
-content.appendChild(menuPage())
-
-
+content.appendChild(menuPage());
+content.appendChild(contactPage());
